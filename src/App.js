5d56d75d5a5a25aa726ba761018a199507078f08
janasara <psarankrish@gmail.com>
2020-01-27 22:39:59 +0530

    initial changes

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ImageFileUpload from './ImageFileUpload';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ImageFileUpload />
+      </header>
+    </div>
+  );
+}
+
+export default App;
