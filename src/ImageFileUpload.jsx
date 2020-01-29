f6a4cf78a46991d95643b700ece9f540dbcda2cf
janasara <psarankrish@gmail.com>
2020-01-29 09:40:13 +0000

    Fileupload changes

--- src/ImageFileUpload.jsx
+++ src/ImageFileUpload.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import axios, { post } from 'axios';
 
 
-
 class ImageFileUpload extends React.Component {
 
   constructor(props) {
@@ -10,7 +9,8 @@ class ImageFileUpload extends React.Component {
     this.state = {
       file: null,
       imgSrc:null,
-      productInfo:"",
+      productName:"",
+      productUrl:"",
     }
     this.onFormSubmit = this.onFormSubmit.bind(this)
     this.onChange = this.onChange.bind(this)
@@ -18,52 +18,72 @@ class ImageFileUpload extends React.Component {
   }
   onFormSubmit(e) {
     e.preventDefault() // Stop form submit
-    this.fileUpload(this.state.file).then((response) => {
+    if (this.state.imgSrc){
+    this.fileUpload(this.state.imgSrc).then((response) => {
       console.log(response.data);
-      this.setState({productInfo:response.data})
+      const prodDtl=`Product Name: ${response.data[0].productClass} `
+      const prodUrl= response.data[0].productUrl
+      this.setState({productName:prodDtl, productUrl:prodUrl})
     })
+    }
   }
+  toBase64 = file => {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = error => reject(error);
+    });
+  };
   onChange(e) {
-    const lfile = e.target.files[0];
-    this.setState({ file: lfile })
-   
-  const lreader = new FileReader();
-  const lurl = lreader.readAsDataURL(lfile);
+    const lfile = e.target.files[0]; 
+    if (!lfile) return 
+    const spltLfile=lfile['name'].split('.')
+    const fileExtn=spltLfile[1]
 
-   lreader.onloadend = function (e) {
-      this.setState({
-          imgSrc: [lreader.result]
-      })
-    }.bind(this);
-  console.log(lurl) //
+  if (fileExtn=='jpg'|| fileExtn=='jpeg'){
+    this.toBase64(lfile).then(imgBase64=>{
+      //console.log("imagBase64:",imgBase64)
+      this.setState({ file: lfile,imgSrc:imgBase64, productName:"",productUrl:""})
+    })
+  } else{
+    this.setState({ file: null,imgSrc:null, productName:"",productUrl:""})
   }
-  fileUpload(file) {
-    const url = 'http://bookmystuff-docker-app-bullseye.apps.cluster.infosysadmcoe.com/';
+  }
+  fileUpload(imgSrc) {
+    const url = 'http://localhost:5000/product';
     const formData = new FormData();
-    formData.append('file', file)
+    const splImage=imgSrc.split(',')
+    const secondBase64=splImage[1]
+    //console.log("secondBase64",secondBase64)
+    formData.append('product_image', secondBase64)
     const config = {
       headers: {
         'content-type': 'multipart/form-data'
       }
     }
     return post(url, formData, config)
+  
   }
 
   render() {
-    console.log("imgSrc:",this.state.imgSrc)
-    console.log("file:",this.state.file)
+    //console.log("imgSrc:",this.state.imgSrc)
+    //console.log("file:",this.state.file)
     return (
       <div>
       <form onSubmit={this.onFormSubmit}>
         <h1>Image File Upload</h1>
         <div>
-          <input type="file" onChange={this.onChange} />
+          <input type="file" onChange={this.onChange} accept="image/jpeg,image/jpg" />
           <button type="submit">Search</button>
         </div>
         </form>
       <div className="image-div">
-       <span> {this.state.productInfo}   </span>    
+       <span> {this.state.productName}   </span>    
       </div>
+      {this.state.productUrl  &&<div className="image-div">          
+       Product URL: <a href={this.state.productUrl} target="_blank"> {this.state.productUrl}</a>   
+      </div>}
       <div className="image-div">
         <img src={this.state.imgSrc} />       
       </div>
